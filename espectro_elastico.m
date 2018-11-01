111192bfbf8a2afa71d85bf646786452041ee392
ppizarror <pablo.pizarro@ing.uchile.cl>
2018-11-01 20:17:33 +0100

    Agrega cálculo del espectro elástico

---
+++ espectro_elastico.m
@@ -0,0 +1,91 @@
+function [t, sa] = espectro_elastico(categoria, zona, suelo, tmax, doplot, tmaxplot)
+%ESPECTRO_ELASTICO Calcula el espectro elastico.
+%
+%   [t, sa] = espectro_elastico(categoria, zona, suelo);
+%   [t, sa] = espectro_elastico(categoria, zona, suelo, tmax);
+%   [t, sa] = espectro_elastico(categoria, zona, suelo, tmax, doplot);
+%   [t, sa] = espectro_elastico(categoria, zona, suelo, tmax, doplot, tmaxplot);
+%
+% En donde:
+%   categoria       Categoria (1, 2, 3, 4) o (i, ii, iii,iv)
+%   zona            Zona sismica (1, 2, 3)
+%   suelo           Tipo suelo (A, B, C, D, E)
+%   graficar        Indica si muestra el grafico al finalizar
+%   tmax            Periodo maximo
+%
+% Ejemplo:
+%   [t, sa] = espectro_elastico(1, 3, 'c', 10, true);
+
+%% Inicia variables
+if ~exist('tmax', 'var')
+    tmax = 10.0;
+end
+if ~exist('doplot', 'var')
+    doplot = false;
+end
+if ~exist('tmaxplot', 'var')
+    tmaxplot = 3.0;
+end
+
+%% Obtiene categoria (importancia)
+categoria = lower(categoria);
+if categoria == 1 || categoria == 'i'
+    imp = 0.6;
+elseif categoria == 1 || categoria == 'ii' %#ok<BDSCA>
+    imp = 1.0;
+elseif categoria == 1 || categoria == 'iii' %#ok<BDSCA>
+    imp = 1.2;
+elseif categoria == 1 || categoria == 'iv' %#ok<BDSCA>
+    imp = 1.2;
+else
+    error('Categoria desconocida');
+end
+
+%% Obtiene aceleracion
+zona = lower(zona);
+if zona == 1
+    a0 = 0.2;
+elseif zona == 2
+    a0 = 0.3;
+elseif zona == 3
+    a0 = 0.4;
+else
+    error('Zona sismica desconocida');
+end
+a0 = a0 * 9.80665;
+
+%% Obtiene parametro de suelo
+[s, t0, ~, ~, p] = sparam(suelo);
+
+%% Genera arreglo de periodo
+maxelems = 1000;
+t = linspace(0, tmax, maxelems)';
+sa = zeros(1, maxelems)';
+
+%% Calcula el espectro
+for i = 1:maxelems
+    sa(i) = s * a0 * imp * (1 + 4.5 * (t(i) / t0)^p) / (1 + (t(i) / t0)^3);
+end
+
+%% Grafica el espectro
+if doplot
+    plot(t, sa, 'k', 'LineWidth', 1.0);
+    title('Espectro de Diseño Nch433 DS.61');
+    xlabel('Periodo T [s]', 'interpreter', 'latex');
+    ylabel('Aceleracion Espectral ($m/s^2$)', 'interpreter', 'latex');
+    grid on;
+    grid minor;
+    xlim([0, tmaxplot]);
+end
+
+%% Guarda los resultados
+fileID = fopen('esp-el.txt', 'w');
+for i = 1:maxelems
+    fprintf(fileID, '%f\t%f', t(i), sa(i));
+    if i < maxelems
+        fprintf(fileID, '\n');
+    end
+end
+fclose(fileID);
+
+end
--- sparam.m
+++ sparam.m
@@ -1,6 +1,6 @@
 function [s, t0, t, n, p] = sparam(t)
 %SPARAM Retorna los parametros segun el tipo de suelo, parametros validos:
-%   [s, to, t, n, p] = sparam(t)
+%   [s, t0, t, n, p] = sparam(t);
 %
 % En donde p: A, B, C, D, E
 
@@ -8,31 +8,31 @@ function [s, t0, t, n, p] = sparam(t)
 t = lower(t);
 
 %% Retorna segun tipo de suelo
-if t=='a'
+if t == 'a'
     s = 0.90;
     t0 = 0.15;
     t = 0.20;
     n = 1.0;
     p = 2.0;
-elseif t=='b'
+elseif t == 'b'
     s = 1.0;
     t0 = 0.30;
     t = 0.35;
     n = 1.33;
     p = 1.5;
-elseif t=='c'
+elseif t == 'c'
     s = 1.05;
     t0 = 0.40;
     t = 0.45;
     n = 1.40;
     p = 1.6;
-elseif t=='d'
+elseif t == 'd'
     s = 1.20;
     t0 = 0.75;
     t = 0.85;
     n = 1.80;
     p = 1.0;
-elseif t=='e'
+elseif t == 'e'
     s = 1.30;
     t0 = 1.20;
     t = 1.35;
